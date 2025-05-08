01553e1820345b40d41297601f34da6f9415a4e5
uthamprabhu <uthamprabhu1@gmail.com>
2025-05-08 18:26:03 +0530

    react vite skeleton code commit

---
+++ gehcflow/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <div className='w-screen h-screen flex justify-center items-center'>
+      <h1 className=''>Hello world</h1>
+    </div>
+  )
+}
+
+export default App
---
+++ webpack-learner/src/index.js
@@ -0,0 +1,4 @@
+const $ = require("jquery")
+
+$("#target").html("hello world")
+$("#target1").html("hello world 123456789")
